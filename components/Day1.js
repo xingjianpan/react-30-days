5d6213696fcdbc18833a0973e6731807dfbdaf20
deric <github@mail.xingjian.me>
2017-03-21 11:24:32 +0800

    Day 1

--- components/Day1.js
+++ components/Day1.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { StyleSheet, PropTypes, Text, ScrollView } from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet, ScrollView, Text } from 'react-native';
 import ScrollableTabView, { DefaultTabBar, ScrollableTabBar, } from "react-native-scrollable-tab-view";
-
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class Day1 extends Component {
   render() {
@@ -9,20 +9,25 @@ class Day1 extends Component {
       <ScrollableTabView
         style={styles.container}
         renderTabBar={() => <ScrollableTabBar />}
-        tabBarActiveTextColor='#032250'
-        tabBarInactiveTextColor='#B5BD89'
-        tabBarUnderlineColor='#FFCCBC'
+        tabBarActiveTextColor="#032250"
+        tabBarInactiveTextColor="#B5BD89"
+        tabBarUnderlineColor="#FFCCBC"
+        // tabBarPosition='overlayTop'
       >
-        <ScrollView tabLabel="Tab #1">
-          <Text style={styles.errorTextStyle}>
-          </Text>
+        <ScrollView tabLabel="iOS">
+          <Icon name="logo-apple" color="black" size={300} style={styles.icon} />
+          <Icon name="ios-phone-portrait" color="black" size={300} style={styles.icon} />
+          <Icon name="logo-apple" color="#DBDDDE" size={300} style={styles.icon} />
+          <Icon name="ios-phone-portrait" color="#DBDDDE" size={300} style={styles.icon} />
         </ScrollView>
-        <Text tabLabel="Tab #2">
-          Tab 2
-        </Text>
-        <Text tabLabel="Tab #3">Tab 3</Text>
-
-
+        <ScrollView tabLabel="Android">
+          <Icon name="logo-android" color="#A4C639" size={300} style={styles.icon} />
+          <Icon name="logo-android" color="black" size={300} style={styles.icon} />
+          <Icon name="logo-android" color="brown" size={300} style={styles.icon} />
+        </ScrollView>
+        <Text tabLabel="tab #3">Tab 3</Text>
+        <Text tabLabel="tab #4">Tab 4</Text>
+        <Text tabLabel="tab #5">Tab 5</Text>
       </ScrollableTabView>
     );
   }
@@ -30,22 +35,11 @@ class Day1 extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     paddingVertical: 1,
     paddingHorizontal: 5,
     flex: 1,
     marginTop: 64,
-  },
-  button: {
-    backgroundColor: "white",
-    borderWidth: 1,
-    borderColor: "black",
-    padding: 10,
-  },
-  errorTextStyle: {
-    fontSize: 15,
-    alignSelf: "center",
-    color: "black",
   },
 });
 
--- router/index.js
+++ router/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { StyleSheet,StatusBar } from 'react-native';
-import { Scene, Reducer, Router, Switch, TabBar, Modal, Schema, Actions } from 'react-native-router-flux';
-import Welcome from '../components/Welcome';
+import { StyleSheet, StatusBar } from 'react-native';
+import { Scene, Router } from 'react-native-router-flux';
+import Main from '../components/Main';
 import Day1 from '../components/Day1';
-import MyScene from '../components/MyScene';
+import Day2 from '../components/Day2';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={styles.container}>
-      <Scene title="30 days of React" key="welcome" component={Welcome} />
+      <Scene title="30 days of React" key="welcome" component={Main} />
       <Scene title="Day-1" key="day1" component={Day1} />
-      <Scene title="Day-2" key="day2" component={MyScene} />
+      <Scene title="Day-2" key="day2" component={Day2} />
     </Router>
   );
 };

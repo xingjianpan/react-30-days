2ee96e745b7b5bad3339d7a8a2793619b932032d
Xingjian <i@xingjian.me>
2017-03-25 13:04:51 +0800

    update reducer

--- reducers/Day4Reducers.js
+++ reducers/Day4Reducers.js
@@ -1,8 +1,8 @@
 
 const INITIAL_STATE = {
   started: false,
-  startTime: '',
-  stopTime: '',
+  startTime: 0,
+  stopTime: 0,
   logs: [],
   watingForAction: true,
 };
@@ -13,12 +13,18 @@ export default (state = INITIAL_STATE, action) => {
     case 'start':
       return { ...state, startTime: action.payload, started: true };
     case 'stop':
-      return { ...state, stopTime: action.payload, started: false, watingForAction: true };
+      return {
+        ...state,
+        stopTime: action.payload,
+        started: false,
+        watingForAction: true,
+        logs: [...state.logs, action.payload - state.startTime],
+      };
     case 'reset':
       return INITIAL_STATE;
     case 'logCurrent':
       return state;
     default:
-      return { ...state, logs: [state.logs, action.payload] };
+      return { ...state };
   }
 };

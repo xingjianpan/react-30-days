b1ca0e18e497ad272956b1c1d1bc57c98e0c10df
deric <github@mail.xingjian.me>
2017-03-30 11:05:01 +0800

    refactor

---
+++ components/Day5/WeatherInfo.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Util from '../../utils';
+
+const WeatherInfo = ({ item }) => {
+  return (
+    <View>
+      <View style={styles.summary}>
+        <Text style={styles.summaryText}>{item.info}</Text>
+      </View>
+
+      <View style={styles.details}>
+        <View style={styles.detailRow}>
+          <Text style={styles.detailRowText}>日出:</Text>
+          <Text style={styles.detailRowValue}>{item.rise}</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <Text style={styles.detailRowText}>日落:</Text>
+          <Text style={styles.detailRowValue}>{item.down}</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <Text style={styles.detailRowText}>降雨概率:</Text>
+          <Text style={styles.detailRowValue}>{item.prop}</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <Text style={styles.detailRowText}>湿度:</Text>
+          <Text style={styles.detailRowValue}>{item.humi}</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <Text style={styles.detailRowText}>风速:</Text>
+          <Text style={styles.detailRowValue}>{item.speed}</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <Text style={styles.detailRowText}>体感温度:</Text>
+          <Text style={styles.detailRowValue}>{item.feel}</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <Text style={styles.detailRowText}>降水量:</Text>
+          <Text style={styles.detailRowValue}>{item.rain}</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <Text style={styles.detailRowText}>气压:</Text>
+          <Text style={styles.detailRowValue}>{item.pres}</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <Text style={styles.detailRowText}>能见度:</Text>
+          <Text style={styles.detailRowValue}>{item.sight}</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <Text style={styles.detailRowText}>紫外线指数:</Text>
+          <Text style={styles.detailRowValue}>{item.uv}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = {
+
+  summary: {
+    marginTop: 3,
+    borderBottomColor: 'rgba(255,255,255,0.7)',
+    borderBottomWidth: Util.pixel,
+  },
+  summaryText: {
+    color: '#fff',
+    fontSize: 16,
+    padding: 5,
+  },
+  details: {
+    paddingTop: 10,
+  },
+
+  detailRow: {
+    flexDirection: 'row',
+    paddingLeft: 50,
+    paddingRight: 50,
+    paddingBottom: 5,
+  },
+  detailRowText: {
+    flex: 1,
+    fontSize: 16,
+    color: '#fff',
+    textAlign: 'right',
+    paddingRight: 10,
+  },
+  detailRowValue: {
+    flex: 1,
+    fontSize: 16,
+    color: '#fff',
+    textAlign: 'left',
+  },
+};
+
+export default WeatherInfo;
---
+++ components/Day5/WeatherTop.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Util from '../../utils';
+
+const WeatherTop = ({ item }) => {
+  return (
+    <View style={styles.topContainer}>
+      <View style={styles.city}>
+        <Text style={styles.cityText}>{item.city}</Text>
+        <Text style={styles.airQualityText}>{item.abs}</Text>
+      </View>
+      <View style={styles.temperature}>
+        <Text style={styles.temperatureText}>{item.degree}°</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = {
+  topContainer: {
+    paddingTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  city: {
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cityText: {
+    fontSize: 30,
+    color: 'white',
+  },
+  airQualityText: {
+    fontSize: 15,
+    color: 'white',
+  },
+  temperature: {
+    paddingTop: 5,
+  },
+  temperatureText: {
+    fontSize: 80,
+    color: 'white',
+  },
+}
+export default WeatherTop;

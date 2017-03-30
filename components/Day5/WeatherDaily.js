28ef1b2ee5725435becb42461461908d7d4e8d4a
deric <github@mail.xingjian.me>
2017-03-30 10:48:21 +0800

    refactor

---
+++ components/Day5/WeatherDaily.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Util from '../../utils';
+
+const WeatherDaily = ({ data }) => {
+  return (
+    <View style={styles.dailyData}>
+      {data.map(item => <DailyItem item={item} key={item.key} />)}
+    </View>
+  );
+};
+
+const DailyItem = ({ item }) => {
+  return (
+    <View style={styles.dailyItemContainer}>
+      <Text style={styles.dailyItemText} >{item.day}</Text>
+      <Icon
+        name={item.icon}
+        size={25}
+        style={styles.DailyItemIcon}
+      />
+      <View style={styles.dailyItemDegree}>
+        <Text style={styles.dailyItemDegreeHigh}>{item.high}</Text>
+        <Text style={styles.dailyItemDegreeLow}>{item.low}</Text>
+      </View>
+    </View>
+  );
+};
+
+
+const styles = {
+  dailyData: {
+    marginTop: 3,
+    borderBottomColor: 'rgba(255,255,255,0.7)',
+    borderBottomWidth: Util.pixel,
+  },
+  dailyItemContainer: {
+    flexDirection: 'row',
+    width: Util.size.width,
+  },
+  dailyItemText: {
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    paddingLeft: 20,
+    color: '#fff',
+  },
+  dailyItemDegree: {
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    paddingRight: 10,
+  },
+
+  DailyItemIcon: {
+    color: '#fff',
+  },
+  dailyItemDegreeHigh:{
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '200',
+    width: 30,
+  },
+  dailyItemDegreeLow: {
+    fontSize: 16,
+    color: '#eee',
+    fontWeight: '200',
+    width: 30,
+  },
+};
+
+export default WeatherDaily;
---
+++ components/Day5/WeatherHourly.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Text, View, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Util from '../../utils';
+
+const HourlyItem = ({ item }) => {
+  return (
+    <View style={styles.hourlyItemContainer}>
+      <Text style={styles.hourlyItemText} >{item.time}</Text>
+      <Icon
+        name={item.icon}
+        size={25}
+        style={[styles.hourlyItemIcon, { color: item.color }]}
+      />
+      <Text style={styles.hourlyItemDegree}>{item.degree}</Text>
+    </View>
+  );
+};
+
+const WeatherHouly = ({ data }) => {
+  return (
+    <ScrollView horizontal style={styles.hourelyDataContainer}>
+      {data.map(item => <HourlyItem item={item} key={item.key} />)}
+    </ScrollView>
+  );
+};
+
+
+const styles = {
+  hourelyDataContainer: {
+    marginTop: 3,
+    borderTopColor: 'rgba(255,255,255,0.7)',
+    borderTopWidth: Util.pixel,
+    borderBottomColor: 'rgba(255,255,255,0.7)',
+    borderBottomWidth: Util.pixel,
+  },
+  hourlyItemContainer: {
+    paddingLeft: 7,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingRight: 10,
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+  },
+  hourlyItemText: {
+    color: '#fff',
+    fontSize: 12,
+    textAlign: 'center'
+  },
+  hourlyItemIcon: {
+    textAlign: 'center',
+    paddingTop: 10,
+  },
+  hourlyItemDegree: {
+    color: '#fff',
+    fontSize: 14,
+    paddingTop: 10,
+    textAlign: 'center',
+  },
+};
+
+export default WeatherHouly;

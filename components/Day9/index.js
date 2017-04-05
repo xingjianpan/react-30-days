6399e2a1399767c75ba1c12a706f6b5f83264082
deric <github@mail.xingjian.me>
2017-04-05 15:23:53 +0800

    add day9

--- components/Day9/index.js
+++ components/Day9/index.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableWithoutFeedback, Animated, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import Button from 'react-native-button';
+import { tap, reset } from '../../actions/Day8';
 
 const ACTION_TIMER = 400;
 const COLORS = ['rgb(255,255,255)', 'rgb(111,235,62)'];
+
 
 class AnimatedButtionPress extends Component {
 
@@ -13,6 +17,7 @@ class AnimatedButtionPress extends Component {
       pressAction: new Animated.Value(0),
       buttonWidth: 100,
       buttonHeight: 20,
+      timerId: null,
     };
   }
 
@@ -26,6 +31,9 @@ class AnimatedButtionPress extends Component {
       duration: ACTION_TIMER,
       toValue: 1,
     }).start(this.animationActionComplete.bind(this));
+    this.state.timerId = setInterval(() => {
+      this.props.tap();
+    }, 10);
   }
 
 
@@ -34,6 +42,7 @@ class AnimatedButtionPress extends Component {
       duration: this.animatedValue * ACTION_TIMER,
       toValue: 0,
     }).start();
+    clearInterval(this.state.timerId);
   }
 
   animationActionComplete() {
@@ -74,20 +83,39 @@ class AnimatedButtionPress extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableWithoutFeedback
-          onPressIn={this.handlePressIn.bind(this)}
-          onPressOut={this.handlePressOut.bind(this)}
-        >
-          <View
-            style={styles.button}
-            onLayout={(e) => { console.log(e); }}
-          >
-            <Animated.View
-              style={[styles.bgFill, this.getProgressStyle()]}
-            />
-            <Text style={styles.text}>Press and hold me</Text>
+        <View style={styles.header}>
+          <Text style={styles.headerText}> Counter</Text>
+        </View>
+
+        <View style={styles.main}>
+          <Text style={styles.numberText}>
+            {this.props.counter}
+          </Text>
+          <View style={styles.buttonGroup}>
+            <TouchableWithoutFeedback
+              onPressIn={this.handlePressIn.bind(this)}
+              onPressOut={this.handlePressOut.bind(this)}
+            >
+              <View
+                style={styles.button}
+                onLayout={(e) => { console.log(e); }}
+              >
+                <Animated.View
+                  style={[styles.bgFill, this.getProgressStyle()]}
+                />
+                <Text style={styles.text}>Press and hold me</Text>
+              </View>
+            </TouchableWithoutFeedback>
+
+            <Button
+              containerStyle={styles.buttonContainer}
+              styleDisabled={{ color: 'red' }}
+              onPress={this.props.reset.bind(this)}
+            >
+              <Text style={styles.buttonText}>Reset</Text>
+            </Button>
           </View>
-        </TouchableWithoutFeedback>
+        </View>
 
         <View >
           <Text >{this.state.textComplete}</Text>
@@ -119,7 +147,50 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
   },
+  header: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 30,
+    color: '#000',
+  },
+  main: {
+    flex: 5,
+    // borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  buttonContainer: {
+    padding: 10,
+    height: 80,
+    width: 80,
+    overflow: 'hidden',
+    borderRadius: 50,
+    justifyContent: 'center',
+  },
+  numberText: {
+    fontSize: 150,
+    fontWeight: '200',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'blue',
+
+  },
 });
 
 
-export default AnimatedButtionPress;
+function mapStateToProps(state) {
+  const { counter } = state.day8;
+  return {
+    counter,
+  };
+}
+export default connect(mapStateToProps, { tap, reset })(AnimatedButtionPress);
